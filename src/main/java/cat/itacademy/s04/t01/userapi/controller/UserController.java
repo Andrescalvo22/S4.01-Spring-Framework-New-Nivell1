258f0a64d6dc4e5757375d2898defc2204c1569d
Andres <andrescalvo01@gmail.com>
2025-10-30 10:31:41 +0100

    Adding Exceptions to project

--- src/main/java/cat/itacademy/s04/t01/userapi/controller/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controller/UserController.java
@@ -4,6 +4,8 @@ package cat.itacademy.s04.t01.userapi.controller;
 import cat.itacademy.s04.t01.userapi.models.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import cat.itacademy.s04.t01.userapi.exceptions.UserNotFoundException;
+
 
 import java.util.*;
 
@@ -13,13 +15,14 @@ import java.util.*;
 public class UserController {
     private static final List<User> users = new ArrayList<>();
 
+    @GetMapping
     public List<User> getAllUsers(@RequestParam(required = false) String name) {
-        if (name == null) {
+        if (name == null || name.isBlank()) {
             return users;
         } else {
             String lowerName = name.toLowerCase();
             return users.stream()
-                    .filter(u -> u.getName().toLowerCase().contains(lowerName))
+                    .filter(u -> u.getName() != null && u.getName().toLowerCase().contains(lowerName))
                     .toList();
         }
 
